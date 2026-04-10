c63638e68e4cfecfa30ddf749ac0cf3bc610e52d
Mohamed Ashraf <157126553+0MohamedAshraf@users.noreply.github.com>
2026-04-10 17:49:49 +0200

    fixed dependencies of navigation and added coil image loader dependency (#5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,5 +65,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation(libs.navigation.compose)
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
 }
